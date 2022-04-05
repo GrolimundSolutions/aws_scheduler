294227408a27e8722b90bcf14b6568d7188c64fe
AchimGrolimund <ac.grolimund@gmail.com>
2022-04-05 19:16:32 +0200

    Update Readme Add more Tests

--- cmd/scheduler/schedulermain/util_test.go
+++ cmd/scheduler/schedulermain/util_test.go
@@ -31,3 +31,29 @@ func Test_getActuallyHour(t *testing.T) {
 		})
 	}
 }
+
+func Test_getDayOfWeek(t *testing.T) {
+	type args struct {
+		loc *time.Location
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				loc: time.Local,
+			},
+			want: int(time.Now().In(time.Local).Weekday()),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getDayOfWeek(tt.args.loc); got != tt.want {
+				t.Errorf("getDayOfWeek() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
